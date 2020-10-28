474a042792716ba5c7d9cda0163ba2656036a408
ejaakml <eximindotong@yahoo.com>
2020-10-28 21:48:02 +0700

    27

---
+++ src/pboif2/pkg10119061/latihan27/hurufbesarhurufkecil/PBOIF210119061Latihan27HurufBesarHurufKecil.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119061.latihan27.hurufbesarhurufkecil;
+
+/**
+ *
+ * @author Rheiza
+ * NAMA  : Rheiza Akaml R
+ * KELAS : IF2   
+ * NIM   : 10119061
+ * 
+ */
+public class PBOIF210119061Latihan27HurufBesarHurufKecil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    String asli = "aPa AkU Di MarS";
+ 
+        String hurufBesar = asli.toUpperCase();
+        String hurufKecil = asli.toLowerCase();
+ 
+        System.out.println("String awal: " + asli);
+        System.out.println("String huruf besar: " + hurufBesar);
+        System.out.println("String huruf kecil: " + hurufKecil);
+    
+    
+    }
+    
+}
